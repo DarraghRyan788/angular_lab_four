ac1616443f5a1c350d4d3a25ede8ec77ce1e0d16
unknown <darraghryan788@gmail.com>
2022-05-03 21:31:16 +0100

    lab upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'data-binding-app';
+  numberOfTime: number = 0;
+  hidden = false;
+
+  count() {
+    this.numberOfTime++;
+  }
+
+  show() {
+    if (this.hidden == true) {
+      this.hidden = false;
+    } else {
+      this.hidden = true;
+    }
+
+  }
 }
